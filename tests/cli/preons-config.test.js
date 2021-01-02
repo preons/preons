60f19ca7529ea72d44a9342fffe65a6cd4d19e0a
Gemma Black <gblackuk@googlemail.com>
2021-01-02 20:20:59 +0000

    fix: original yaml stringification

--- tests/cli/preons-config.test.js
+++ tests/cli/preons-config.test.js
@@ -10,7 +10,7 @@ it.each([["preons-config-input.yaml", "preons-config-output.json"]])(
     "converting a preons config into an expanded config for seeing resolved classes for each css property",
     async (inputFile, outputFile) => {
         let { stdout } = await pExec(
-            `src/console/preons config tests/cli/examples/${inputFile}`
+            `src/console/preons config -a tests/cli/examples/${inputFile}`
         )
 
         let result = fs.readFileSync(`tests/cli/examples/${outputFile}`, {
